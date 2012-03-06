dc09559817865e70c905fcc64af1bb2cc2e658d1
Simon Harrer <simon.harrer@gmail.com>
2012-03-06 14:07:42 +0100

    Copying 4 files

--- lib/textools/commands.rb
+++ lib/textools/commands.rb
@@ -1,4 +1,5 @@
 require "thor"
+require 'thor/group'
 
 module Textools
 
@@ -6,17 +7,22 @@ module Textools
     include Thor::Actions
 
     desc "create PROJECT_NAME", "create a new LaTeX project"
-    def generate(name)
+    def create(name)
       # create containing directory
       inside name do |folder|
-        copy_file ".gitignore",File.join(folder,".gitignore")
+        [".gitignore","clean.bat","clean.rb","clean.sh"].each do |file|
+          copy_file file, File.join(folder, file)
+        end
       end
     end
 
+    private
 
     # set the starting point of the templates search to the lib/textools/templates folder
     def self.source_root
       File.join(File.dirname(__FILE__),"templates")
     end
+
+  end
 
 end
